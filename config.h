dfcdc97c60532788e19e9b940004315f03287bfd
fengwk <759543714@qq.com>
2022-01-30 18:38:14 +0800

    update

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "FiraCode Nerd Font Mono:size=12:antialias=true:autohint=true";
-static char *font = "SauceCodePro Nerd Font Mono:pixelsize=30:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font Mono:pixelsize=32:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
